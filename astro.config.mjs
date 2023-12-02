b0957a21d5c6d5dfaa0dac6e87e1367174403e93
OctavioQuiroz <octavioquiroz30@gmail.com>
2023-12-01 20:16:24 -0600

    fix: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://raloz.github.io/',
+    site: 'https://raloz.github.io',
     base: '/raloz.github.io'
 });
