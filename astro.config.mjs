427b406187351694d0e59f59e8b7c9a51a53a842
OctavioQuiroz <octavioquiroz30@gmail.com>
2023-12-03 22:40:50 -0600

    feat: adding cool stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://raloz.github.io',
-    base: '/raloz.github.io'
+  site: 'https://raloz.github.io',
+  integrations: [tailwind()]
 });
